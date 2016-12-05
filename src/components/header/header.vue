c439affad3bcb8c35c5131804f4686f86fe4ed82
Junru Guo <guojunru1989@126.com>
2016-12-05 18:09:18 +0800

    test

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,7 +13,7 @@
                     {{seller.description}}/{{seller.deliveryTime}}分钟送达
                 </div>
                 <div class="support" v-if="seller.supports">
-                    <span class="icon"></span>
+                    <span class="icon" :class="classMap[seller.supports[0].type]"></span>
                     <span class="text">{{seller.supports[0].description}}</span>
                 </div>
             </div>
@@ -28,6 +28,10 @@
             seller: {
                 type: Object
             }
+        },
+
+        created () {
+        	this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special']
         }
     }
 </script>
@@ -42,6 +46,9 @@
             font-size: 0
             .avatar
                 display: inline-block
+                vertical-align: top
+                img
+                    border-radius 2px
             .content
                 display: inline-block
                 font-size: 14px
@@ -61,4 +68,26 @@
                         font-size: 16px
                         line-height: 18px
                         font-weight: bold
+                .description
+                    margin-bottom: 10px
+                    line-height: 12px
+                    font-size 12px
+                .support
+                    .icon
+                        display inline-block
+                        width: 12px
+                        height: 12px
+                        margin-right: 4px
+                        background-size: 12px 12px
+                        background-repeat: no-repeat
+                        &.decrease
+                            bg-image('decrease_1')
+                        &.discount
+                            bg-image('discount_1')
+                        &.guarantee
+                            bg-image('guarantee_1')
+                        &.invoice
+                            bg-image('invoice_1')
+                        &.special
+                            bg-image('special_1')
 </style>
